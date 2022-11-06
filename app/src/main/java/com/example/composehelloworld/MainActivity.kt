7e280006eeeccf6cdb6ffb805078ba77d8bc2de3
Allison McEntire <allisonpaigemcentire@gmail.com>
2022-11-06 11:25:06 -0500

    hello bill murray

---
+++ app/src/main/java/com/example/composehelloworld/MainActivity.kt
@@ -0,0 +1,138 @@
+package com.example.composehelloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = MaterialTheme.colors.background) {
+                Greeting(name = "Bill Muuray")
+                imageFromURL()
+                TextFieldScreen()
+                MyButton()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier.padding(1.dp),
+        fontStyle = FontStyle.Italic,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun imageFromURL() {
+    // on below line we are creating a column,
+    Column(
+        // in this column we are adding modifier
+        // to fill max size, mz height and max width
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+            .fillMaxWidth()
+            // on below line we are adding
+            // padding from all sides.
+            .padding(1.dp),
+        // on below line we are adding vertical
+        // and horizontal arrangement.
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // on below line we are adding image for our image view.
+        Image(
+            // on below line we are adding the image url
+            // from which we will  be loading our image.
+            painter = rememberAsyncImagePainter("https://www.fillmurray.com/200/300"),
+
+            // on below line we are adding content
+            // description for our image.
+            contentDescription = "gfg image",
+
+            // on below line we are adding modifier for our
+            // image as wrap content for height and width.
+            modifier = Modifier
+                .wrapContentSize()
+                .wrapContentHeight()
+                .wrapContentWidth()
+        )
+    }
+}
+
+@Composable
+fun TextFieldScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        MyTextField()
+    }
+}
+
+@Composable
+fun MyTextField() {
+    val textValue = remember { mutableStateOf("") }
+
+    val primaryColor = colorResource(id = R.color.colorPrimary)
+
+    OutlinedTextField(
+        label = { Text(text = stringResource(id = R.string.email)) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = primaryColor,
+            focusedLabelColor = primaryColor,
+            cursorColor = primaryColor
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+        },
+    )
+}
+
+@Composable
+fun MyButton() {
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.colorPrimary)),
+        border = BorderStroke(
+            1.dp,
+            color = colorResource(id = R.color.colorPrimaryDark)
+        )
+    ) {
+        Text(
+            text = stringResource(id = R.string.button_text),
+            color = colorResource(id = R.color.white)
+        )
+    }
+}
+
+
+
